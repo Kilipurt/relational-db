696c970100d2b3b0f7a2fce45c373ee7c7251250
Kilipurt <proandrey15@gmail.com>
2018-06-13 16:21:27 +0300

    lesson2

---
+++ src/lesson2/Demo.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+public class Demo {
+    public static void main(String[] args) {
+        //java
+        //create class with fields
+        //create object with some values
+        //using it
+
+        //db
+        //create table with columns
+        //insert some data to the table
+        //using it
+    }
+}
