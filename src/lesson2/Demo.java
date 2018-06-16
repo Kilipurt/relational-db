905c3fc7030138abb351c6e1211b2e20b84d0a33
Kilipurt <proandrey15@gmail.com>
2018-06-16 19:50:23 +0300

    lesson3 & homework

--- src/lesson2/Demo.java
+++ src/lesson2/Demo.java
@@ -1,11 +1,15 @@
 package lesson2;
 
+import java.util.Date;
+
 public class Demo {
     public static void main(String[] args) {
         //java
         //create class with fields
         //create object with some values
         //using it
+
+        Order order = new Order(1001, "tool2212", 140, new Date(), new Date());
 
         //db
         //create table with columns
---
+++ src/sql/INSERT_RECORDS_INTO_ORDERS_TABLE.sql
@@ -0,0 +1,29 @@
+INSERT INTO ORDERS
+VALUES(100, 'test1', 10, TO_DATE('2018/06/16 19:18:00', 'yyyy/mm/dd hh24:mi:ss'), TO_DATE('2018/06/16 20:18:00', 'yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(101, 'test2', 20, TO_DATE('2018/06/16 19:18:00', 'yyyy/mm/dd hh24:mi:ss'), TO_DATE('2018/06/16 20:18:00', 'yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(102, 'test3', 30, TO_DATE('2018/06/16 19:18:00', 'yyyy/mm/dd hh24:mi:ss'), TO_DATE('2018/06/16 20:18:00', 'yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(103, 'test4', 40, TO_DATE('2018/06/16 19:18:00', 'yyyy/mm/dd hh24:mi:ss'), TO_DATE('2018/06/16 20:18:00', 'yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(104, 'test5', 50, TO_DATE('2018/06/16 19:18:00', 'yyyy/mm/dd hh24:mi:ss'), TO_DATE('2018/06/16 20:18:00', 'yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(105, 'test6', 60, TO_DATE('2018/06/16 19:18:00', 'yyyy/mm/dd hh24:mi:ss'), TO_DATE('2018/06/16 20:18:00', 'yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(106, 'test7', 70, TO_DATE('2018/06/16 19:18:00', 'yyyy/mm/dd hh24:mi:ss'), TO_DATE('2018/06/16 20:18:00', 'yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(107, 'test8', 80, TO_DATE('2018/06/16 19:18:00', 'yyyy/mm/dd hh24:mi:ss'), TO_DATE('2018/06/16 20:18:00', 'yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(108, 'test9', 90, TO_DATE('2018/06/16 19:18:00', 'yyyy/mm/dd hh24:mi:ss'), TO_DATE('2018/06/16 20:18:00', 'yyyy/mm/dd hh24:mi:ss'));
+
+INSERT INTO ORDERS
+VALUES(109, 'test10', 100, TO_DATE('2018/06/16 19:18:00', 'yyyy/mm/dd hh24:mi:ss'), TO_DATE('2018/06/16 20:18:00', 'yyyy/mm/dd hh24:mi:ss'));
